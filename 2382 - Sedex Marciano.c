06a14bf400786549cc03e8770af4a00cd1d1836f
MrRique15 <henrique-favaro@hotmail.com>
2022-08-02 10:26:29 -0300

    Solved

---
+++ 2382 - Sedex Marciano.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+    float largura, altura, profundidade, raioEsfera, larguraEsfera;
+    double diagonalCaixa;
+
+    scanf("%f %f %f %f", &largura, &altura, &profundidade, &raioEsfera);
+
+    larguraEsfera = 2 * raioEsfera;
+    diagonalCaixa = sqrt(pow(largura, 2) + pow(altura, 2) + pow(profundidade, 2));
+    if(diagonalCaixa <= larguraEsfera){
+        printf("S\n");
+    }else{
+        printf("N\n");
+    }
+    return 0;
+}
