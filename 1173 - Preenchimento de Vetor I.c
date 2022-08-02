6363451bf6c70a1ebae52ddb83d5077d1e38fd2c
MrRique15 <henrique-favaro@hotmail.com>
2022-08-02 08:53:38 -0300

    Solved

--- 1173 - Preenchimento de Vetor I.c
+++ 1173 - Preenchimento de Vetor I.c
@@ -5,10 +5,10 @@ int main(){
     int a;
     scanf("%d", &a);
     X[0] = a;
-    printf("X[0] = %d\n", X[0]);
+    printf("N[0] = %d\n", X[0]);
     for(int i = 1; i < 10; i++){
         X[i] = 2*X[i-1];
-        // printf("X[%d] = %d\n", i, X[i]);
+        printf("N[%d] = %d\n", i, X[i]);
     }
     return 0;
 }
